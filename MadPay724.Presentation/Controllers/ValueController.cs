7b256b2f8d623614def51dfe3828442d7fe2bd4c
hmdmhmd <hmd.tradeinfo@gmail.com>
2020-05-13 00:32:44 +0430

    first commit

---
+++ MadPay724.Presentation/Controllers/ValueController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MadPay724.Presentation.Controllers
+{
+    [Route("api/[controller]s")]
+    [ApiController]
+    public class ValueController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+
+            return "hamid";
+        }
+    }
+}
